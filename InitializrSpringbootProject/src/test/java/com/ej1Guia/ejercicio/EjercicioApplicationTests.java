4f0f3a4215061307b210343c869e228ffd6f6f76
JuanCJ9688 <ironjohn788@gmail.com>
2023-11-16 20:06:38 -0500

    prueba

---
+++ InitializrSpringbootProject/src/test/java/com/ej1Guia/ejercicio/EjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ej1Guia.ejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
